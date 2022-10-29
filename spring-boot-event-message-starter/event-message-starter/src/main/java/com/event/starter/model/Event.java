63a3d87b50bd2a215bb771cbd028537a6b02f82d
it <email@e.com>
2022-10-29 23:06:27 +0300

    add custom starter

---
+++ spring-boot-event-message-starter/event-message-starter/src/main/java/com/event/starter/model/Event.java
@@ -0,0 +1,14 @@
+package com.event.starter.model;
+
+import lombok.ToString;
+import lombok.Value;
+
+/**
+ * Это контейнер, который будет аккумулировать данные,
+ * полученные во время обработки событий.
+ * @param type тип события
+ * @param payload полезные данные(некоторая дополнительная информация)
+ */
+@ToString
+public record Event(String type, String payload) {
+}
---
+++ spring-boot-event-message-starter/event-message-starter/src/test/java/com/event/starter/EventMessageStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMessageStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
